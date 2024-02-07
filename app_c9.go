33f39062b8a7448cb8ee7eba2487d71c7254d1a1
ZgoAsma <asma.zgolli@axelspringer.com>
2024-02-07 11:30:48 +0100

    Create app_c9.go

---
+++ app_c9.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var timeZone = map[string]int{
+		"UTC": 0 * 60 * 60,
+		"EST": -5 * 60 * 60,
+		"CST": -6 * 60 * 60,
+		"MST": -7 * 60 * 60,
+		"PST": -8 * 60 * 60,
+	}
+	fmt.Println(timeZone)
+
+	attended := map[string]bool{
+		"Ann": true,
+		"Joe": true,
+	}
+
+	person := "Asma"
+	if attended[person] { // will be false if person is not in the map
+		fmt.Println(person, "was at the meeting")
+	}
+}
