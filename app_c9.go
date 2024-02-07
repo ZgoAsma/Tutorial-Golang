41eff6d9082ebc9996d1209acc38ab10a13beee3
ZgoAsma <asma.zgolli@axelspringer.com>
2024-02-07 14:47:17 +0100

    Update app_c9.go

--- app_c9.go
+++ app_c9.go
@@ -21,4 +21,23 @@ func main() {
 	if attended[person] { // will be false if person is not in the map
 		fmt.Println(person, "was at the meeting")
 	}
+
+	fmt.Println(aggregate(2, 3, 4, add))
+	// prints 9
+	fmt.Println(aggregate(2, 3, 4, mul))
+	// prints 24
+
+}
+
+func add(x, y int) int {
+	return x + y
+}
+
+func mul(x, y int) int {
+	return x * y
+}
+
+// aggregate applies the given math function to the first 3 inputs
+func aggregate(a, b, c int, arithmetic func(int, int) int) int {
+	return arithmetic(arithmetic(a, b), c)
 }
